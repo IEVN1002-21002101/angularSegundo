79b9647631de5f816f747724b03bdb438a93d2e8
Alfredo Emmanuel Rosas Largo <Alfredo.rs1896@gmail.com>
2024-10-12 10:28:20 -0600

    rutas

---
+++ src/app/auth/features/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from "@angular/router";
+ 
+export default[
+    {
+        path: 'sign-in',
+        loadComponent:()=> import('./sign-in/sign-in.component'),
+    },
+    {
+        path: 'sign-up',
+        loadComponent:()=> import('./sign-up/sign-up.component'),
+    },
+    {
+        path: '*',
+        redirectTo: ''
+    },
+] as Routes
+ 
--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -10,13 +10,20 @@ interface Usuarios{
 @Component({
   selector: 'app-ejemplo1',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './ejemplo1.component.html',
   styleUrl: './ejemplo1.component.css'
 })
 export class Ejemplo1Component implements OnInit {
   formGroup!:FormGroup;
 
+  materia='pwa'
+  tem=''
+  alumnos:Usuarios={
+    nombre:'',
+    edad: 0,
+    email:''
+  }
   constructor(private fb:FormBuilder){}
 
   ngOnInit(): void{
@@ -28,6 +35,28 @@ export class Ejemplo1Component implements OnInit {
       edad:[''],
       email:[''],
     })
+  }
+
+  onSubmit():void{
+    const{nombre,edad,email}=this.formGroup.value;
+    this.alumnos.nombre=nombre;
+    this.alumnos.edad=edad;
+    this.alumnos.email=email;
+    let alumnoJSON=JSON.stringify(this.alumnos);
+
+    console.log(this.formGroup.value);
+
+    localStorage.setItem('materia',this.materia);
+    localStorage.setItem('alumno',alumnoJSON);
+  }
+
+  subImprimir():void{
+    this.tem=localStorage.getItem('materia')!
+
+    const alumnoGuardado=localStorage.getItem('alumno')
+    if(alumnoGuardado){
+      const alumno:Usuarios=JSON.parse(alumnoGuardado)
+    }
   }
 
 }
